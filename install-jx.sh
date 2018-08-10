7a773f9233e0fbf3be0148516b7aac55a090eba9
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-08-10 15:33:41 +0100

    fix: improved install script to use a ~/bin dir

--- install-jx.sh
+++ install-jx.sh
@@ -5,16 +5,27 @@ GIT_USER="Jenkins-X"
 GIT_EMAIL="tutorial@jenkins-x.io"
 
 function install_dependencies() {
-	wget -q https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
-	tar xvf jx-linux-amd64.tar.gz >/dev/null 2>&1
-	rm jx-linux-amd64.tar.gz >/dev/null 2>&1
-	echo "export PATH=$PATH:$PWD"
+    wget -q https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
+    tar xvf jx-linux-amd64.tar.gz >/dev/null 2>&1
+    rm jx-linux-amd64.tar.gz >/dev/null 2>&1
+    mkdir -p ~/bin
+    mv jx ~/bin
+}
+
+function add_path_to_bashrc() {
+    if grep -q PATH ~/.bashrc; then
+        # do nothing
+    else
+        echo "export PATH=$PATH:$HOME/bin" >> ~/.bashrc
+        echo "export PATH=$PATH:$HOME/bin"
+    fi
 }
 
 function configure_environment() {
-	git config --global --add user.name "${GIT_USER}"
-	git config --global --add user.email "${GIT_EMAIL}"
+    git config --global --add user.name "${GIT_USER}"
+    git config --global --add user.email "${GIT_EMAIL}"
 }
 
 install_dependencies
 configure_environment
+add_path_to_bashrc
