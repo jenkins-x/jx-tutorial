05b37ca32e0b361d430b33e745347a22a85a292d
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-12-07 09:56:45 +0000

    feat: removed the setting of git config as jx handles this for us now

--- install-jx.sh
+++ install-jx.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 JX_VERSION=1.3.631
-GIT_USER="Jenkins-X"
-GIT_EMAIL="tutorial@jenkins-x.io"
 
 function install_dependencies() {
     wget -q https://github.com/jenkins-x/jx/releases/download/v${JX_VERSION}/jx-linux-amd64.tar.gz
@@ -21,11 +19,5 @@ function add_path_to_bashrc() {
     fi
 }
 
-function configure_environment() {
-    git config --global --add user.name "${GIT_USER}"
-    git config --global --add user.email "${GIT_EMAIL}"
-}
-
 install_dependencies
-configure_environment
 add_path_to_bashrc
